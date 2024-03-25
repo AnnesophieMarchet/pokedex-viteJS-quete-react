1e1d828a5039c7f5036a23ba8ce77a2257dc2d0e
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-03-25 17:08:26 +0100

    style PokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -5,6 +5,15 @@ function PokemonCard() {
         <img
           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
           alt="bulbazaur"
+          className="avatar"
+          style={{
+            width: "100%",
+            height: "auto",
+            marginBottom: "0.5rem",
+            backgroundColor: "black",
+            boxShadow: "inherit",
+            borderRadius: "20px",
+          }}
         />
         <figcaption>Bulbasaur</figcaption>
       </figure>
